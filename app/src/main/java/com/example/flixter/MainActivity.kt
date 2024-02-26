b86c6a5294e147301d6a13a17fec7cdeee0aac02
Deja Carter <dejacarter005@gmail.com>
2024-02-25 20:50:03 -0500

    Initial Comment

--- app/src/main/java/com/example/flixter/MainActivity.kt
+++ app/src/main/java/com/example/flixter/MainActivity.kt
@@ -2,13 +2,39 @@ package com.example.flixter
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.flixter.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_movies -> {
+                switchFragment(MovieFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_persons -> {
+                switchFragment(PersonFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    private fun switchFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.content, fragment).commit()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val supportFragmentManager = supportFragmentManager
-        val fragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.content, MovieFragment(), null).commit()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+        // Initially display the MovieFragment
+        switchFragment(MovieFragment())
     }
 }
